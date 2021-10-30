f2b55db10f6e522e54ea85917e1c6c4941f0d205
vasapavan1109 <76104116+vasapavan1109@users.noreply.github.com>
2021-10-30 11:52:46 +0530

    Add files via upload

---
+++ cloudstorage.py
@@ -0,0 +1,25 @@
+import dropbox
+class TransferData:
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f.read(), file_to)
+
+def main():
+    access_token = 'ZKUjEnERpSEAAAAAAAAAAdJk5tHlz2OkuFamuS5rpx9zV_-6yJeQzbKhcu0cJoWf'
+    transferData = TransferData(access_token)
+
+    file_from = 'text.txt'
+    file_to = '/home/text.txt'  # The full path to upload the file to, including the file name
+
+    # API v2
+    transferData.upload_file(file_from, file_to)
+
+if __name__ == '__main__':
+    main()
